1b30a9702d9fb4d53d6d2be4d98597cfcbbbadfd
ClausJohansen <clausfjohansen@gmail.com>
2016-11-22 14:06:51 +0100

    Buttons exercise

---
+++ Buttons/Buttons/Buttons/Buttons/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Buttons
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Button_OnClick(object sender, EventArgs e)
+        {
+            Button invoker = (Button)sender;
+            string inputCharacter = invoker.Text;
+
+            if (string.IsNullOrEmpty(NumberLabel.Text))
+                NumberLabel.Text = "";
+
+            NumberLabel.Text += inputCharacter;
+        }
+    }
+}
